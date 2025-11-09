208e6cfa6ec02266e7d522197a7c86f6bde883de
wes337 <wesmoses@gmail.com>
2025-11-09 18:57:27 +0200

    viewport fit

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,12 @@ export const metadata = {
   description: "The official BLCKK website",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  viewportFit: "cover",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
